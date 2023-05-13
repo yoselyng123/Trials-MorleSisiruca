f63d0fd07dab903995cbb748f660aab6a369ccbb
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2023-05-13 10:56:46 -0400

    Blank page

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <p> This is a blank page </p>
+    </main>
+  );
+}
